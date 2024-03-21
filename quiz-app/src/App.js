2580959358a3c103411b1a9717105cfe0e799d7a
swarna1822 <146362837+swarna1822@users.noreply.github.com>
2024-03-21 08:05:39 +0530

    questions created changes

---
+++ quiz-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import questions from './constants/questions.json'
+
+function App() {
+  return (
+    <h1>world quiz</h1>
+    // {questionsComponent}
+    //{resultsComponent}
+  );
+}
+
+export default App;
